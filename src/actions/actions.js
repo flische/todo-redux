43bf0b24ac15bc4461b9a2931e5bfcec5823c8dc
flische <flische@gmail.com>
2018-08-23 15:53:27 -0700

    added functionality to display list data

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,15 @@
 import types from './types';
+import axios from 'axios';
 
+const BASE_URL = 'http://api.reactprototypes.com';
 const API_KEY = '?key=c618_demofed';
+
+export function getAllListData(){
+    const response = axios.get(`${BASE_URL}/todos${API_KEY}`);
+
+    return {
+        type: types.GET_ALL_LIST_DATA,  // <-- this is just setting a string to the type property!
+        payload: response               // <-- certain 'middleware' expects you to use 'payload'
+    };
+}
+
--- src/components/list.js
+++ src/components/list.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getAllListData } from '../actions/actions';
 
 class List extends Component {
+
     render(){
+        const listElements = this.props.list.map ( item => {
+            return <li key={item._id} className="collection-item">{item.title}</li>
+        });
+
         return (
-            <ul className="addCollection">
-                <li className="collection-item">This will be the todo list</li>
+            <ul className="collection">
+                {listElements}
             </ul>
         )
     }
+    componentDidMount(){
+        this.props.getAllListData();
+    }
+
 }
 
-export default List;
+function mapStateToProps(state){
+    return {
+        list: state.list.all // 'state.list' refers to reducers.js // 'list.all' refers to list_reducer.js
+    }
+}
+
+export default connect( mapStateToProps, { getAllListData: getAllListData, } )(List);
+// connect takes the stuff from above and adds it to props. Which we can now pass and use in componentDidMount
+
+// export default connect( mapStateToProps, actionCreators )(List);

122c9ab890d18001919ea796432ab95023fb6623
flische <flische@gmail.com>
2018-08-23 12:58:49 -0700

    displaying list in progress

---
+++ src/components/list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    render(){
+        return (
+            <ul className="addCollection">
+                <li className="collection-item">This will be the todo list</li>
+            </ul>
+        )
+    }
+}
+
+export default List;

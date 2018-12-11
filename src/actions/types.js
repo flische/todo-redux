f9f34dba113a5b7708805639527fd385b8352338
flische <flische@gmail.com>
2018-12-10 16:45:34 -0800

    added action types delete and toggle complete

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,7 @@
 export default {
     ADD_ITEM: 'add_item',
-    GET_ALL_LIST_DATA: 'get_all_list_data'
+    DELETE_ITEM: 'delete_item',
+    GET_ALL_LIST_DATA: 'get_all_list_data',
+    TOGGLE_COMPLETE: 'toggle_complete'
 };
 
